9379620aa1b5186d19c5167e20f27d326d6d77f0
alexpapadopoulos <apapadopoulos@deloitte.gr>
2022-07-17 22:59:40 +0300

    product and shop handling on product screen

--- site/src/mainscreens/ProductListScreen.js
+++ site/src/mainscreens/ProductListScreen.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Grid } from '@mui/material'
+import { Container, Grid, Typography } from '@mui/material'
 import ProductCard from '../components/Product/ProductCard'
 import axios from 'axios'
 import { useLocation } from 'react-router-dom'
+import { useTranslation } from 'react-i18next';
 
 const GridProductItem = ({photoDir, productName, productPrice}) => {
   return (
-    <Grid item xs={12} sm={6} md={4} lg={3}>
+    <Grid item xs={12} sm={6} md={4} lg={4}>
       <ProductCard photoDir={photoDir} productName={productName} productPrice={productPrice}></ProductCard>
     </Grid>
   )
@@ -15,11 +16,16 @@ const GridProductItem = ({photoDir, productName, productPrice}) => {
 const ProductListScreen = () => {
   const location = useLocation()
   const shopId = location.pathname.split('/')[2]
+
+  const { t } = useTranslation()
+
   const [products, setProducts] = useState([])
+  const [shop, setShop] = useState({})
   const getProducts = async () => {
     try {
       const { data } = await axios('http://localhost:9000/api/shop/'+shopId)
-      setProducts(data)
+      setProducts(data.products)
+      setShop(data.shop)
     } catch (e) {
       console.log(e.message)
     }
@@ -30,10 +36,11 @@ const ProductListScreen = () => {
     
   , [])
   return (<>
-    <Container maxWidth='xl' >
-      <Grid container spacing={4} style={{ marginTop: 'unset' }}>
+    <Container maxWidth='md' >
+      <Typography component="h5" variant="h5" sx={{pt:2}}>{t('welcomeTo')} {shop.name}</Typography>
+      <Grid container spacing={2} style={{ marginTop: 'unset' }}>
         {
-          products.map(prod => <GridProductItem photoDir={prod.photoDir} productName={prod.name} productPrice={prod.price} />)
+          products.map(prod => <GridProductItem key={prod.id} photoDir={prod.photoDir} productName={prod.name} productPrice={prod.price} />)
         }
       </Grid>
     </Container>
