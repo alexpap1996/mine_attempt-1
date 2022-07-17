cfdc2d685e0adff36e73ad8fc0f6e2d9dffb2cf0
alexpapadopoulos <apapadopoulos@deloitte.gr>
2022-07-17 22:59:08 +0300

    quantity selector icons

--- site/src/components/Product/QuantitySelector.js
+++ site/src/components/Product/QuantitySelector.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import { Box, IconButton, Typography } from "@mui/material"
+import { Add, Remove } from '@mui/icons-material';
 
 const QuantitySelector = ({quantity = 0, size }) => {
-  const fontSize = size === 'medium' ? '1.3rem' : '1rem'
+  const fontSize = size === 'medium' ? '1.5rem' : '1.3rem'
+
   const [currQuantity, setCurrQuantity] = useState(quantity)
   const incrementHandler = () => {
     setCurrQuantity(currQuantity + 1)
@@ -10,14 +12,15 @@ const QuantitySelector = ({quantity = 0, size }) => {
   const decrementHandler = () => {
     if (currQuantity > 1) setCurrQuantity(currQuantity - 1)
   }
+
   return (
     <Box sx={{ display: 'flex', alignItems:'center'}}>
-      <IconButton color='secondary' sx={{backgroundColor: 'darkgrey', height: fontSize, width: fontSize }} onClick={decrementHandler}>
-        <p style={{fontSize}}>-</p>
+      <IconButton color='secondary'  sx={{backgroundColor: 'darkgrey', height: fontSize, width: fontSize }} onClick={decrementHandler}>
+        <Remove fontSize='small'/>
       </IconButton>
         <Typography variant='body1' sx={{ padding: '0 0.3rem', fontSize}}>{currQuantity}</Typography>
       <IconButton color='secondary' sx={{backgroundColor: 'darkgrey', height: fontSize, width: fontSize }} onClick={incrementHandler}>
-        <p style={{fontSize}}>+</p>
+        <Add  fontSize='small'/>
       </IconButton>
     </Box>
   )
