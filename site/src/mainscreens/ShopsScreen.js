ae7a35cfeea5544244b5788da529d04316d42396
alexpapadopoulos <apapadopoulos@deloitte.gr>
2022-07-20 03:27:22 +0300

    shop screen cleanup

--- site/src/mainscreens/ShopsScreen.js
+++ site/src/mainscreens/ShopsScreen.js
@@ -1,5 +1,4 @@
-import { Box } from '@mui/material';
-import React, { useState } from 'react';
+import React from 'react';
 import { useLocation } from 'react-router-dom'
 import CategoryList from '../components/ShopCategories/CategoryList';
 import ShopList from '../components/Shops/ShopList';
