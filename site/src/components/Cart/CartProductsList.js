b8e33416acc3f0514b85b5e2ad9472c128a79f94
alexpapadopoulos <apapadopoulos@deloitte.gr>
2022-07-20 03:25:00 +0300

    empty cart component

--- site/src/components/Cart/CartProductsList.js
+++ site/src/components/Cart/CartProductsList.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { List, Divider, Card } from '@mui/material';
+import { List, Divider, Card, Box, Typography } from '@mui/material';
 import ProductCartItem from '../Product/ProductCartItem';
+import EmptyCartIcon from '@mui/icons-material/RemoveShoppingCartTwoTone';
+import { useTranslation } from 'react-i18next';
 
 const ProductWithDivider = ({product, isLastItem = false}) => {
   return (
@@ -11,20 +13,40 @@ const ProductWithDivider = ({product, isLastItem = false}) => {
   )
 }
 
+const EmptyCart = ({text}) => {
+  return (
+    <Box sx={{
+      display: 'flex',
+      p:5,
+      justifyContent:
+      'center', 
+      alignItems:'center', 
+      flexDirection: 'column'}}
+    >
+      <EmptyCartIcon sx={{width:'4rem', height:'4rem', pb:3 }}/>
+      <Typography variant='h6' component='div'>{text}</Typography>
+    </Box>
+  )
+}
+
 const CartProductsList = ({ cartItems }) => {
+  const { t } = useTranslation()
   return <>
     <Card sx={{bgcolor: 'white' }}>
-      <List sx={{ width: '100%', bgcolor: 'background.paper', padding: '0' }}>
-        {
-          cartItems.map((product, index, arr) => 
-            <ProductWithDivider 
-              key={product.id}
-              product={product} 
-              isLastItem={index+1=== arr.length}
-            />
-          )
-        }
-      </List>
+      {cartItems.length ? 
+        <List sx={{ width: '100%', bgcolor: 'background.paper', padding: '0' }}>
+          {
+            cartItems.map((product, index, arr) => 
+              <ProductWithDivider 
+                key={product.id}
+                product={product} 
+                isLastItem={index+1=== arr.length}
+              />
+            )
+          }
+        </List>
+        : <EmptyCart text={t('noCartItems')}/>
+      }
     </Card>
   </>
 }
