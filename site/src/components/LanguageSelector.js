6247467aac7d39b9e45ad885b81d1768d375720b
alexpapadopoulos <apapadopoulos@deloitte.gr>
2022-07-20 20:57:27 +0300

    unused import cleanup

--- site/src/components/LanguageSelector.js
+++ site/src/components/LanguageSelector.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import TextField from '@mui/material/TextField'
 import MenuItem from '@mui/material/MenuItem'
 import { useTranslation } from "react-i18next";
