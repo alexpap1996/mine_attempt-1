fba1c4e9f703f84aa5a47382ef20dbbf39eb7539
alexpapadopoulos <apapadopoulos@deloitte.gr>
2022-08-07 21:36:35 +0300

    ORDERS: list with grid together

--- site/src/components/Orders/OrdersList.js
+++ site/src/components/Orders/OrdersList.js
@@ -1,4 +1,4 @@
-import { ListItem, List } from '@mui/material'
+import { ListItem, List, ListItemButton, Grid, Typography, Divider } from '@mui/material'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
@@ -17,27 +17,37 @@ const OrderListItem = ({order, products}) => {
   const locale = currLang === 'gr' ? 'el-GR' : 'en-US'
 
   const displayPrice = Number.parseInt(order.price || 0).toFixed(2)
+  const displayDate = date.toLocaleDateString(locale, options)
 
-  return (
-  <ListItem>
-    <p>{date.toLocaleDateString(locale, options)} {displayPrice} f</p>
-    {
-      products
-        .filter(prod => order.products.includes(prod.id))
-        .map(prod => 
-          <p>{prod.id}</p>
-      )
-    }
-  </ListItem>)
+  return (<>
+    <Divider />
+    
+    <ListItem sx={{padding:'0'}}>
+      <ListItemButton component="a" href="#simple-list" sx={{p:3}}>
+        <Grid container>
+          <Grid item xs={9}>
+            <Typography>
+              {displayDate}
+            </Typography>
+          </Grid>
+          <Grid item xs={3}>
+            <Typography sx={{ textAlign:'right' }}>
+              {displayPrice}€
+            </Typography>
+          </Grid>
+        </Grid>
+      </ListItemButton>
+    </ListItem>
+  </>)
 }
 
 const OrdersList = ({orders = [], products = []}) => {
   return (<>
-    <List sx={{ width: '100%', bgcolor: 'background.paper', padding: '0' }}>  
+    <List spacing={2} sx={{ width: '100%', padding:'0'}}>  
       {
         orders.map(order => <OrderListItem order={order} products={products}/>)
       }
     </List>
-  </>)
+    </>)
 }
 export default OrdersList
