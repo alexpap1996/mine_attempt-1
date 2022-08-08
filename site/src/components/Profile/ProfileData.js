40b0835290d775a72c9354e6a1c88aeb4bc4c12c
alexpapadopoulos <apapadopoulos@deloitte.gr>
2022-08-08 20:04:15 +0300

    PROFILE: view only mode done

---
+++ site/src/components/Profile/ProfileData.js
@@ -0,0 +1,29 @@
+import { Grid, TextField } from "@mui/material"
+import { useTranslation } from 'react-i18next'
+
+const GridItem = ({value, label}) => {
+  return <Grid item xs={12} md={6}>
+    <TextField
+      label={label}
+      defaultValue={value}
+      InputProps={{
+        readOnly: true,
+      }}
+      variant="standard"
+    />    
+  </Grid>
+}
+
+const ProfileData = ({ user = {} }) => {
+  const { t } = useTranslation()
+  return (<>
+    <Grid container spacing={2} sx={{p:2}}>
+      <GridItem label={t('firstname')} value={user.firstname} />
+      <GridItem label={t('lastname')} value={user.lastname} />
+      <GridItem label={t('emergencyPhone')} value={user.emergencyphone} />
+      <GridItem label={t('emailAddress')} value={user.username} />
+    </Grid>
+  </>)
+}
+
+export default ProfileData
--- site/src/mainscreens/ProfileScreen.js
+++ site/src/mainscreens/ProfileScreen.js
@@ -1,24 +1,30 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { ENDPOINT } from '../constants/routeConstants'
-import { Container, Card, CardContent, Grid, Typography } from '@mui/material'
+import { Container, Card, CardContent, Grid, Typography, Box } from '@mui/material'
+import { GlobalState } from '../contexts/Context'
+import ProfileData from '../components/Profile/ProfileData'
+import { useTranslation } from 'react-i18next'
 
 const ProfileScreen = () => {
-  
+  const { t } = useTranslation()
+  const { state: { user }} = GlobalState()
+
 	return <>
   	<Container maxWidth='sm' sx={{pt:2 }}> 
-			<Card spacing={2} style={{ marginTop:'unset' }} >
-      <CardContent>
+			<Card spacing={2} columnSpacing={2} style={{ marginTop:'unset' }} >
+        <CardContent>
 					<Grid container sx={{ justifyContent:'center' }}>
 						<Grid item xs={12} sx={{pb: 2}}>
 							<Typography component="h4" variant="h4" sx={{textAlign: 'left', pt:1, pl:1}}>
-								Your Profile
+								{t('profile')}
 							</Typography>
 							<Typography component="div" variant="body1" sx={{textAlign: 'left', pl:1}}>
-								Here you can see your saved information
+								{t('seeProfileInfo')}
 							</Typography>
 						</Grid>
-					</Grid>	
+					</Grid>
+          <ProfileData user={user}></ProfileData>
 				</CardContent>
       </Card>
     </Container>
