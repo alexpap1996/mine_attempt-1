74b9ce4d9e75e739d0d5b0f8f27c39eea2c68e30
alexpapadopoulos <apapadopoulos@deloitte.gr>
2022-08-10 01:31:43 +0300

    SERVER_CTRL: choose db from mongo cluster

--- server/config/database.js
+++ server/config/database.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose'
 // make it work with local mongo db
 const connect = async () => {
   try {
-    const uri = `mongodb+srv://alexpap:paok_gasp1234@cluster0.61xvxwf.mongodb.net/?retryWrites=true&w=majority`
+    const uri = `mongodb+srv://alexpap:paok_gasp1234@cluster0.61xvxwf.mongodb.net/ecommerce?retryWrites=true&w=majority`
     const conn = await mongoose.connect(uri, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
