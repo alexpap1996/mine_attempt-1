8150ff70d136fa4cbc3a8d3661a296b00b4c1130
alexpapadopoulos <apapadopoulos@deloitte.gr>
2022-08-10 17:18:15 +0300

    CART: validation on cart screen

--- site/src/mainscreens/CartScreen.js
+++ site/src/mainscreens/CartScreen.js
@@ -1,14 +1,24 @@
-import { Grid } from '@mui/material';
-import { Container } from '@mui/system';
-import React from 'react';
+import { Grid, Container } from '@mui/material';
+import React, { useState } from 'react';
 import CartSummary from '../components/Cart/CartSummary';
 import CartProductsList from '../components/Cart/CartProductsList';
 import CartPaymentInfo from '../components/Cart/CartPaymentInfo';
 import { GlobalState } from '../contexts/Context'
 import CartTipSelector from '../components/Cart/CartTipSelector';
 
+const paymentMethodInit = {
+  type: 'cash',
+  details: {
+    cardnumber: '',
+    cardname: '',
+    cvv: '',
+    expirydate: ''
+  }
+}
 const CartScreen = () => {
   const { dispatch, state: { cart, tip } } = GlobalState()
+  const [paymentMethod, setPaymentMethod] = useState(paymentMethodInit)
+  const [paymentDetailsMissing, setPaymentDetailsMissing] = useState('')
 
 	const cartItems = cart
   const handleTipChange = (event) => {
@@ -16,6 +26,28 @@ const CartScreen = () => {
       type: 'tip',
       payload: event.target.value
     })
+  }
+
+  const validatePaymentMethod = (payMethod) => {
+    if (payMethod.type === 'cash') return true
+    else {
+      const details = paymentMethod.details
+      let isOk = true
+      if (details.cardnumber.length !== 16) isOk = false
+      if (details.cardname.length === 0) isOk = false
+      if (details.expirydate.length !== 5) isOk = false
+      if (details.cvv.length !== 3) isOk = false
+      return isOk
+    }
+  }
+
+  const handlePaymentMethodChange = (payMethod) => {
+    setPaymentMethod(payMethod)
+    if (validatePaymentMethod(paymentMethod)) {
+      setPaymentDetailsMissing('')
+    } else {
+      setPaymentDetailsMissing('Missing details')
+    }
   }
   
 	return <>
@@ -30,12 +62,12 @@ const CartScreen = () => {
               <CartTipSelector tip={tip} handleTipChange={handleTipChange} hasCartItems={cartItems.length} />
             </Grid>
             <Grid xs={12} sm={12} md={6} item>
-              <CartPaymentInfo hasCartItems={cartItems.length}/>
+              <CartPaymentInfo hasCartItems={cartItems.length} handlePaymentMethodChange={handlePaymentMethodChange} detailsMissing={paymentDetailsMissing}/>
             </Grid>
           </Grid>
         </Grid>
         <Grid xs={12} sm={12} md={4} item>
-          <CartSummary hasCartItems={cartItems.length}/>
+          <CartSummary hasCartItems={cartItems.length} detailsMissing={paymentDetailsMissing} paymentMethod={paymentMethod.type}/>
         </Grid>
       </Grid>
     </Container >
