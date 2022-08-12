d2cd17d2e1c14f72485810703ac53110ec77fb86
gaspalex2011@gmail.com <gaspalex2011@gmail.com>
2022-08-12 18:43:39 +0300

    SEEDER: fixes

--- server/dataForFirstTime.js
+++ server/dataForFirstTime.js
@@ -1,4 +1,3 @@
-import mongoose from 'mongoose'
 import Product from './schemas/productSchema.js'
 import Shop from './schemas/shopSchema.js'
 import User from './schemas/userSchema.js'
@@ -14,14 +13,6 @@ class Name {
   }
 }
 
-class _Shop {
-  constructor(name, category, image) {
-    this.name = name,
-    this.category = category,
-    this.image = image
-  }
-}
-
 const createShop = (name, category, image) => {
   const shop = new Shop()
   shop.name = name,
@@ -30,55 +21,68 @@ const createShop = (name, category, image) => {
   return shop
 }
 
-class _User {
-  constructor (firstname, lastname, email, password, emergencyphone) {
-    this.firstname = firstname
-    this.lastname = lastname
-    this.email = email
-    this.password = password
-    this.emergencyphone = emergencyphone
-  }
+const createProd = (name, shopId, price, image) => {
+  const prod = new Product()
+  prod.name = name
+  prod.shopId = shopId
+  prod.price = price
+  prod.image = image
+  return prod
 }
 
-class _Product { 
-  constructor (name, shopId, price, image) {
-    this.name = name
-    this.shopId = shopId
-    this.price = price
-    this.image = image
-  }
+const createUser = (firstname, lastname, email, password, emergencyphone) => {
+  const user = new User()
+  user.firstname = firstname
+  user.lastname = lastname
+  user.email = email
+  user.password = password
+  user.emergencyphone = emergencyphone
 }
 
 // delete all existing records from database
 const deleteExistingData = async () => {
   await Product.deleteMany({})
-  await User.deleteMany({})
+  // await User.deleteMany({})
   await Shop.deleteMany({})
 }
 
 const createUsers = async () => {
-  const user1 = new _User('firstname', 'lastname', 'test@test.com', 'password', '6912345678')
+  const user1 = createUser('firstname', 'lastname', 'test@test.com', 'password', '6912345678')
+  const users = [user1]
+  const res = await User.bulkSave(users)
+  return res
 }
 
+// shops that are missing and would be nice to have
+// 2 bakeries (1 big 1 small maybe)
+// 2 butchers (1 normal butcher 1 fish shop)
+// 
 const createShops = async () => {
-  const market1 = new _Shop(new Name('Mini Market', 'Μίνι Μάρκετ'), 'groceries', 'https://previews.123rf.com/images/filmlandscape/filmlandscape1702/filmlandscape170200008/72252201-a-famous-mini-mart-on-the-local-walking-street-of-luang-prabang-laos-.jpg')
-  const pharmacy = new _Shop(new Name('Pharmacy', 'Φαρμακειο'), 'pharmacies', 'https://res.cloudinary.com/djuuwduyx/image/upload/v1658335845/pharmacy_1_ofngw0.jpg')
-  const gyroplace = new _Shop(new Name('Gyro Place', 'Γυράδικο'), 'food', 'https://res.cloudinary.com/djuuwduyx/image/upload/v1658337282/gyro_1_afzo80.jpg')
-  const crepeplace = new _Shop(new Name('Crepe Place', 'Κρεπερί'), 'food', 'https://res.cloudinary.com/djuuwduyx/image/upload/v1658245464/cld-sample-4.jpg')
+  const market1 = createShop(new Name('Mini Market', 'Μίνι Μάρκετ'), 'groceries', 'https://www.giveandfund.com/giveandfund/projects/360/n50jfbbmhq.jpg')
+  const market2 = createShop(new Name('Grand Market', 'Γκραντ Μάρκετ'), 'groceries', 'https://previews.123rf.com/images/filmlandscape/filmlandscape1702/filmlandscape170200008/72252201-a-famous-mini-mart-on-the-local-walking-street-of-luang-prabang-laos-.jpg')
+  const pharmacy = createShop(new Name('Pharmacy', 'Φαρμακειο'), 'pharmacies', 'https://res.cloudinary.com/djuuwduyx/image/upload/v1658335845/pharmacy_1_ofngw0.jpg')
+  const gyroplace = createShop(new Name('Gyro Place', 'Γυράδικο'), 'food', 'https://res.cloudinary.com/djuuwduyx/image/upload/v1658337282/gyro_1_afzo80.jpg')
+  const crepeplace = createShop(new Name('Crepe Place', 'Κρεπερί'), 'food', 'https://res.cloudinary.com/djuuwduyx/image/upload/v1658245464/cld-sample-4.jpg')
   
-  const shops = [market1, pharmacy, gyroplace, crepeplace]
-  const market2 = createShop(new Name('Mini Market', 'Μίνι Μάρκετ'), 'groceries', 'https://previews.123rf.com/images/filmlandscape/filmlandscape1702/filmlandscape170200008/72252201-a-famous-mini-mart-on-the-local-walking-street-of-luang-prabang-laos-.jpg')
-  const res = await market2.save()
-  // const res = await Shop.save(shops)
-  console.log('res',res)
+  const shops = [market1, market2, pharmacy, gyroplace, crepeplace]
+  const res = await Shop.bulkSave(shops)
+  return res.insertedIds
 }
 
-// missing shopIds, dont use for now
-const createProducts = () => {
-  const orange = new _Product(new Name('Orange', 'Πορτοκάλι'), '', 0.7, 'https://res.cloudinary.com/djuuwduyx/image/upload/v1658245479/orange_d4p0kz.jpg')
-  const vitaminC = new _Product(new Name('Vitamin C - 1000mg', 'Βιταμινη C - 1000μγ'), '', 'https://res.cloudinary.com/djuuwduyx/image/upload/v1660036588/5df9a1287e60e99cc253bd768e7d2236_VITC1000_r2ilyq.jpg')
+// goal: every shop should have at least 5 products for now
+// feel free to clone products and changing only the shopId so the same product shows up at two shops
+const createProducts = async (shopIds) => {
+  const MiniMarketId = shopIds[0]
+  const GrandMarketId = shopIds[1]
+  const pharmacyId = shopIds[2]
+  const gyroplace = shopIds[3]
+  const crepeplace = shopIds[4]
+
+  const orange = createProd(new Name('Orange', 'Πορτοκάλι'), GrandMarketId, 0.7, 'https://res.cloudinary.com/djuuwduyx/image/upload/v1658245479/orange_d4p0kz.jpg')
+  const vitaminC = createProd(new Name('Vitamin C - 1000mg', 'Βιταμινη C - 1000μγ'), pharmacyId, 8.0, 'https://res.cloudinary.com/djuuwduyx/image/upload/v1660036588/5df9a1287e60e99cc253bd768e7d2236_VITC1000_r2ilyq.jpg')
   
   const products = [orange, vitaminC]
+  const res = await Product.bulkSave(products)
 }
 
 // connect to database
@@ -88,9 +92,9 @@ const runApp = async () => {
   await deleteExistingData()
   
   // create the records
-  await createUsers()
-  await createShops()
-  // createProducts()
+  // await createUsers()
+  const shopIds = await createShops()
+  createProducts(shopIds)
 }
 runApp()
 
