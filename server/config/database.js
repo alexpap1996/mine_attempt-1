39101b614faa6e7193bed818fea6e478208095bc
gaspalex2011@gmail.com <gaspalex2011@gmail.com>
2022-08-13 14:39:07 +0300

    DB: comment for uri localhost

--- server/config/database.js
+++ server/config/database.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose'
 // make it work with local mongo db
 const connect = async () => {
   try {
+    //const uri = 
     const uri = `mongodb+srv://alexpap:paok_gasp1234@cluster0.61xvxwf.mongodb.net/ecommerce?retryWrites=true&w=majority`
     const conn = await mongoose.connect(uri, {
       useUnifiedTopology: true,
