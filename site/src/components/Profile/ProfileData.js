d887e7b97a3f3635e9364d79a0e96b06626f35d1
gaspalex2011@gmail.com <gaspalex2011@gmail.com>
2022-08-13 14:39:42 +0300

    PROFILE: field logic for edit

--- site/src/components/Profile/ProfileData.js
+++ site/src/components/Profile/ProfileData.js
@@ -1,30 +1,52 @@
 import { Grid, TextField, FormControl } from "@mui/material"
 import { useTranslation } from 'react-i18next'
 
-const GridItem = ({value, label}) => {
+const GridItem = ({value, label, name, editState, changeValue, type}) => {
   return <Grid item xs={12} md={6}>
     <FormControl fullWidth>
       <TextField
+        name={name}
         label={label}
         defaultValue={value}
         InputProps={{
-          readOnly: true,
+          disabled: !editState,
         }}
         variant="standard"
         sx={{px:3, label:{px:4}, input: {textAlign: "right", pr:1}}}
+        onChange={changeValue}
+        type={type ? type : 'text'}
       />
     </FormControl>
   </Grid>
 }
 
-const ProfileData = ({ user = {} }) => {
+const ProfileData = ({ user = {}, editState, changeValue }) => {
   const { t } = useTranslation()
   return (<>
     <Grid container spacing={2} sx={{p:2}}>
-      <GridItem label={t('firstname')} value={user.firstname} />
-      <GridItem label={t('lastname')} value={user.lastname} />
-      <GridItem label={t('emergencyPhone')} value={user.emergencyphone} />
-      <GridItem label={t('emailAddress')} value={user.email} />
+      <GridItem 
+        label={t('firstname')} 
+        value={user.firstname} 
+        name="firstname" 
+        editState={editState} 
+        changeValue={changeValue}
+      />
+      <GridItem 
+        label={t('lastname')} 
+        value={user.lastname}  
+        name="lastname" 
+        editState={editState} 
+        changeValue={changeValue}
+      />
+      <GridItem 
+        label={t('emergencyPhone')} 
+        value={user.emergencyphone} 
+        name="emergencyphone" 
+        editState={editState} 
+        changeValue={changeValue}
+        type="number"
+      />
+      <GridItem label={t('emailAddress')} value={user.email}/>
     </Grid>
   </>)
 }
