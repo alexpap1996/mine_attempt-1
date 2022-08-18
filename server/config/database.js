864e16d5a58c2c644fcc9e8ce7784f21b182a895
gaspalex2011@gmail.com <gaspalex2011@gmail.com>
2022-08-18 14:55:10 +0300

    temp change for working online

--- server/config/database.js
+++ server/config/database.js
@@ -5,8 +5,8 @@ import mongoose from 'mongoose'
 // make it work with local mongo db
 const connect = async () => {
   try {
-    //const uri = 
-    const uri = `mongodb://0.0.0.0:27017/myShopDB` //`mongodb://localhost:27017` `mongodb+srv://alexpap:paok_gasp1234@cluster0.61xvxwf.mongodb.net/ecommerce?retryWrites=true&w=majority`
+    const uri = `mongodb+srv://alexpap:paok_gasp1234@cluster0.61xvxwf.mongodb.net/ecommerce?retryWrites=true&w=majority`
+    // const uri = `mongodb://0.0.0.0:27017/myShopDB` //`mongodb://localhost:27017` `mongodb+srv://alexpap:paok_gasp1234@cluster0.61xvxwf.mongodb.net/ecommerce?retryWrites=true&w=majority`
     const conn = await mongoose.connect(uri, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
--- site/src/constants/routeConstants.js
+++ site/src/constants/routeConstants.js
@@ -1,4 +1,4 @@
-const ENDPOINT = 'http://localhost:9000' //'http://localhost:9000' 'https://node-alex-pap-1.herokuapp.com'
+const ENDPOINT = 'https://node-alex-pap-1.herokuapp.com' //'http://localhost:9000' //'http://localhost:9000' 'https://node-alex-pap-1.herokuapp.com'
 
 export {
   ENDPOINT
